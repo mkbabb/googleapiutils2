4beefa6f28a8d19880c82cd5848dc9518f6d6bc2
Mike Babb <mike7400@gmail.com>
2022-12-23 14:05:06 -0500

    Updated geocoding

--- googleapiutils/geocode.py
+++ googleapiutils/geocode.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import *
 
 import requests
+from requests.exceptions import JSONDecodeError
 
 from ._types.geocode import *
 from .utils import update_url_params
@@ -14,13 +15,23 @@ class Geocode:
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def geocode(self, address: str) -> list[GeocodeResult]:
+    @staticmethod
+    def _return_if_200(r: requests.Response) -> list[GeocodeResult] | None:
+        if r.status_code == 200:
+            try:
+                return r.json()
+            except JSONDecodeError:
+                return None
+        else:
+            return None
+
+    def geocode(self, address: str):
         url = update_url_params(Geocode.URL, {"address": address, "key": self.api_key})
         r = requests.get(url)
-        return r.json()
+        return self._return_if_200(r)
 
-    def reverse_geocode(self, lat: float, long: float) -> list[GeocodeResult]:
+    def reverse_geocode(self, lat: float, long: float):
         latlng = f"{lat},{long}"
         url = update_url_params(Geocode.URL, {"latlng": latlng, "key": self.api_key})
         r = requests.get(url)
-        return r.json()
+        return self._return_if_200(r)
