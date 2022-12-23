6cf4557140177a3c98efe2a3ca4a9bc9cd629929
Mike Babb <mike7400@gmail.com>
2022-12-23 14:20:01 -0500

    tmp

--- googleapiutils/_types/geocode.py
+++ googleapiutils/_types/geocode.py
@@ -7,37 +7,34 @@ LocationType = Literal[
     "ROOFTOP", "RANGE_INTERPOLATED", "GEOMETRIC_CENTER", "APPROXIMATE"
 ]
 
+if TYPE_CHECKING:
 
-@typing.type_check_only
-class AddressComponents(TypedDict):
-    long_name: str
-    short_name: str
-    types: list[str]
+    @typing.type_check_only
+    class AddressComponents(TypedDict):
+        long_name: str
+        short_name: str
+        types: list[str]
 
+    @typing.type_check_only
+    class Location(TypedDict):
+        lat: float
+        lng: float
 
-@typing.type_check_only
-class Location(TypedDict):
-    lat: float
-    lng: float
+    @typing.type_check_only
+    class ViewPort(TypedDict):
+        northeast: Location
+        southwest: Location
 
+    @typing.type_check_only
+    class Geometry(TypedDict):
+        location: Location
+        location_type: LocationType
+        viewport: ViewPort
 
-@typing.type_check_only
-class ViewPort(TypedDict):
-    northeast: Location
-    southwest: Location
-
-
-@typing.type_check_only
-class Geometry(TypedDict):
-    location: Location
-    location_type: LocationType
-    viewport: ViewPort
-
-
-@typing.type_check_only
-class GeocodeResult(TypedDict):
-    address_components: list[AddressComponents]
-    formatted_address: str
-    geometry: Geometry
-    place_id: str
-    types: list[str]
+    @typing.type_check_only
+    class GeocodeResult(TypedDict):
+        address_components: list[AddressComponents]
+        formatted_address: str
+        geometry: Geometry
+        place_id: str
+        types: list[str]
