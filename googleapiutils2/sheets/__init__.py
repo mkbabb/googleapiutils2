2de3354f76bdeac97b4a1e5d1833153f5843d6f4
Mike Babb <mike7400@gmail.com>
2023-07-10 15:15:53 -0400

    bump

--- googleapiutils2/sheets/__init__.py
+++ googleapiutils2/sheets/__init__.py
@@ -1,4 +1,4 @@
 from .misc import InsertDataOption, SheetSliceT, ValueInputOption, ValueRenderOption
 from .sheets import Sheets
-from .sheets_slice import SheetSlice
+from .sheets_slice import SheetSlice, normalize_sheets_range
 from .sheets_value_range import SheetsValueRange
