589f3357dcce4565b63a460686f5c829c476454b
Mike Babb <mike7400@gmail.com>
2024-06-13 10:08:37 -0400

    tmp

--- examples/runzero.py
+++ examples/runzero.py
@@ -28,6 +28,10 @@ if TYPE_CHECKING:
 logger.add("./log/log.log", rotation="1 MB")
 
 
+def get_size_in_mb(filepath: pathlib.Path) -> float:
+    return round(filepath.stat().st_size / 1024 / 1024, 2)
+
+
 async def create_folder_for_org(o: dict, drive: Drive, base_folder: str):
     name, oid = o["name"], o["id"]
 
@@ -48,14 +52,13 @@ async def create_folder_for_org(o: dict, drive: Drive, base_folder: str):
 
 
 @retry(retries=10, exponential_backoff=True)
-async def download_and_upload(
-    session: ClientSession,
-    base_url: str,
-    headers: dict,
+async def download_endpoint(
     name: str,
     oid: str,
-    folder: dict,
+    session: ClientSession,
+    base_url: str,
     endpoint: str,
+    headers: dict,
 ):
 
     t_filename = endpoint.split("/")[-1]
@@ -63,6 +66,13 @@ async def download_and_upload(
         t_filename = "org_info"
 
     filename = pathlib.Path(t_filename).with_suffix(".json")
+
+    filepath = pathlib.Path(f"./data/{name}/{filename}")
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+
+    if filepath.exists() and filepath.stat().st_size > 0:
+        logger.info(f"Skipping {filename} for {name}: already downloaded")
+        return filepath
 
     params = {}
     if str(filename) == "tasks.json":
@@ -72,51 +82,38 @@ async def download_and_upload(
 
     logger.info(f"Downloading {filename} for {name}...")
 
-    @cache_with_stale_interval()
-    async def get_url(
-        url: str,
-        headers: dict,
-        oid: str,
-        params: dict,
-    ):
+    async with session.get(url, headers=headers, params={"_oid": oid, **params}) as r:
+        if r.status != http.HTTPStatus.OK:
+            raise Exception(
+                f"Failed to download {url} for {name}: {r.status}; {r.reason} {r}"
+            )
 
-        async with session.get(
-            url, headers=headers, params={"_oid": oid, **params}
-        ) as r:
-            if r.status != http.HTTPStatus.OK:
-                raise Exception(
-                    f"Failed to download {url} for {name}: {r.status}; {r.reason} {r}"
-                )
+        logger.info(
+            f"Initial request for {filename} for {name} successful, downloading content..."
+        )
 
-            text = await r.text()
+        with open(filepath, "ab") as f:
+            async for data in r.content.iter_any():
+                f.write(data)
 
-            size_in_mb = round(len(text) / 1024 / 1024, 2)
+        size_in_mb = get_size_in_mb(filepath)
 
-            logger.info(f"Downloading {filename}: size {size_in_mb} MB")
+        logger.info(f"Downloaded {filename} for {name}; size {size_in_mb} MB")
 
-            return text
-
-    text = await get_url(
-        url=url,
-        headers=headers,
-        oid=oid,
-        params=params,
-    )
-
-    filepath = pathlib.Path(f"./data/{name}/{filename}")
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-    filepath.write_text(text)
-
-    return filepath
+        return filepath
 
 
-def compress_files(filepaths: List[pathlib.Path]):
-    name = tempfile.NamedTemporaryFile().name
+def compress_files(
+    filepaths: List[pathlib.Path], name: pathlib.Path | str | None = None
+):
+    if name is None:
+        name = pathlib.Path(tempfile.NamedTemporaryFile().name)
+
+    name = pathlib.Path(name)
 
     with tempfile.TemporaryDirectory() as t_temp_dir:
         temp_dir = pathlib.Path(t_temp_dir)
-
-        tar_filepath = temp_dir / f"{name}.tar"
+        tar_filepath = temp_dir / name.with_suffix(".tar")
 
         # Create a tar file with the JSON files
         with tarfile.open(tar_filepath, "w") as tar:
@@ -124,8 +121,8 @@ def compress_files(filepaths: List[pathlib.Path]):
                 tar.add(filepath, arcname=filepath.name)
 
         # Compress the tar file using gzip
-        gzip_name = f"{name}.tar.gz"
-        gzip_filepath = temp_dir / gzip_name
+        gzip_filepath = temp_dir / name.with_suffix(".tar.gz")
+
         with open(tar_filepath, 'rb') as f_in:
             with gzip.open(gzip_filepath, 'wb') as f_out:
                 f_out.writelines(f_in)
@@ -136,19 +133,6 @@ def compress_files(filepaths: List[pathlib.Path]):
 async def process_org_folder(
     base_url: str, org_folders: dict, endpoints: list, headers: dict
 ):
-    async def create_done_file(folder: File):
-        logger.info(f"Finished processing {folder['name']}: {folder['webViewLink']}...")
-
-        with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
-            f.write(f"Finished at {datetime.now()}")
-
-            drive.upload(
-                filepath=f.name,
-                name="done",
-                to_mime_type=GoogleMimeTypes.txt,
-                parents=folder["id"],
-            )
-
     async with ClientSession() as session:
         for (name, oid), folder in org_folders.items():
 
@@ -157,45 +141,42 @@ async def process_org_folder(
             if (
                 done_file := next(drive.list(folder["id"], query="name = 'done'"), None)
             ) is not None:
-                logger.info(f"Skipping {name}: already processed")
-                continue
+                drive.delete(done_file["id"])
 
             tasks = []
             for endpoint in endpoints:
                 task = asyncio.create_task(
-                    download_and_upload(
+                    download_endpoint(
+                        name=name,
+                        oid=oid,
                         session=session,
                         base_url=base_url,
                         endpoint=endpoint,
-                        oid=oid,
                         headers=headers,
-                        folder=folder,
-                        name=name,
                     )
                 )
                 tasks.append(task)
 
             filepaths = await asyncio.gather(*tasks)
 
-            await create_done_file(folder)
-
             logger.info(f"Compressing {len(filepaths)} files for {name}...")
 
-            gzip_name = f"{name}.tar.gz"
-            gzip_filepath = compress_files(
-                filepaths=filepaths,
+            gzip_filepath = compress_files(filepaths=filepaths, name=name)
+
+            size_in_mb = get_size_in_mb(gzip_filepath)
+
+            logger.info(
+                f"Uploading {gzip_filepath.name}; size {size_in_mb} MB for {name}..."
             )
-
-            size_in_mb = round(gzip_filepath.stat().st_size / 1024 / 1024, 2)
-
-            logger.info(f"Uploading {gzip_name}; size {size_in_mb} MB for {name}...")
 
             drive.upload(
                 filepath=gzip_filepath,
-                name=gzip_name,
+                name=gzip_filepath.name,
                 to_mime_type=GoogleMimeTypes.zip,
                 parents=folder["id"],
             )
+
+            logger.info(f"Uploaded {gzip_filepath.name} for {name}")
 
 
 drive = Drive()
