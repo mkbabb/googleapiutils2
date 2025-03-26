b5fb18e6ff95e774094345f7ddf20391e53471c2
Mike Babb <mike7400@gmail.com>
2025-03-26 13:55:53 -0400

    version bump, examples change

---
+++ examples/mail.py
@@ -0,0 +1,17 @@
+from googleapiutils2 import Mail, get_oauth2_creds, ServiceAccountCredentials
+
+creds: ServiceAccountCredentials = get_oauth2_creds(
+    client_config="auth/credentials.fortinbras.json"
+)
+
+creds = creds.with_subject("mbabb@ridgemontcharter.org")
+
+mail = Mail(creds=creds)
+
+mail.send(
+    sender=creds.signer_email,
+    to="mbabb@ncsu.edu",
+    subject="Test email from Friday Institute",
+    body="This is a test email from Friday Institute using Google API.",
+    user_id="me",
+)
